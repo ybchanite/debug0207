6c777240d0bc70b29f527ce61995c414381a0df4
ybchanite <33406167+ybchanite@users.noreply.github.com>
2018-01-18 11:18:19 +0800

    FAQs restore

--- bootstrap_cityu/cityu/langswitcher.js
+++ bootstrap_cityu/cityu/langswitcher.js
@@ -10,13 +10,16 @@ var lang = jQuery('html').attr('lang');
 
 switch (lang) {
     case 'en':
-        jQuery('<a id="expandButton">Expand All</span>').insertAfter("#views-exposed-form-taught-faq-block-block-1 .form-item-field-faq-category-target-id");
+        jQuery('<a id="expandButton">Expand All</span>').insertAfter("#views-exposed-form-taught-faq-01-page-1 .form-item-field-faq-language-target-id");
         break;
     case 'zh-hant':
-        jQuery('<a id="expandButton">展開全部</span>').insertAfter("#views-exposed-form-taught-faq-block-block-1 .form-item-field-faq-category-target-id");
+        jQuery('<a id="expandButton">展開全部</span>').insertAfter("#views-exposed-form-taught-faq-01-page-1 .form-item-field-faq-language-target-id");
+        break;
+    case 'zh-hans':
+        jQuery('<a id="expandButton">展开全部</span>').insertAfter("#views-exposed-form-taught-faq-01-page-1 .form-item-field-faq-language-target-id");
         break;
     default:
-        jQuery('<a id="expandButton">Expand All</span>').insertAfter("#views-exposed-form-taught-faq-block-block-1 .form-item-field-faq-category-target-id");
+        jQuery('<a id="expandButton">Expand All</span>').insertAfter("#views-exposed-form-taught-faq-01-page-1 .form-item-field-faq-language-target-id");
         break;
 }
 
@@ -25,25 +28,18 @@ var expandAllButton = 0;
 jQuery('#expandButton').click(function(){
 
     if (expandAllButton === 0){
-        jQuery('.view_taught_faq').css('background-color','#863275');
-        jQuery('.view_taught_faq_plus').text("-");
-        jQuery('.views-field-body').show('fast');
+        jQuery('.view-taught-faq-01 .views-field-field-faq-question .view_taught_faq').css('background-color','#863275');
+        jQuery('.view-taught-faq-01 .views-field-field-faq-question .view_taught_faq_plus').text("-");
+        jQuery('.view-taught-faq-01 .views-field-body').show('fast');
         expandAllButton = 1;
     }
     else{
-        jQuery('.view_taught_faq').css('background-color','#aaa4ad');
-        jQuery('.view_taught_faq_plus').text("+");
-        jQuery('.views-field-body').hide('fast');
+        jQuery('.view-taught-faq-01 .views-field-field-faq-question .view_taught_faq').css('background-color','#aaa4ad');
+        jQuery('.view-taught-faq-01 .views-field-field-faq-question .view_taught_faq_plus').text("+");
+        jQuery('.view-taught-faq-01 .views-field-body').hide('fast');
         expandAllButton = 0;
         
     }
         
 
 });
-
-
-
-
-
-
-
