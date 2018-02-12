7103f89b9f746cc8ec607aa5acc6804c8a2c5d48
ybchanite <33406167+ybchanite@users.noreply.github.com>
2018-02-12 11:36:53 +0800

    Update timeline.js

--- bootstrap_cityu/cityu/timeline.js
+++ bootstrap_cityu/cityu/timeline.js
@@ -112,17 +112,18 @@ if (!(sidebar === null)){
         if (jQuery('.timeline-item-wrapper .views-field-field-at-info').css('position') === 'absolute'){
             var offset = sidebar.offset().top;
             sidebar.stop().clearQueue();
+            alert(jQuery(window).scrollTop() > offset);
             if (jQuery(window).scrollTop() > offset) {
                 console.log(jQuery(window).scrollTop());
                 console.log('' + offset);
                 sidebar.animate({
                     marginTop: jQuery(window).scrollTop() - offset + 200
-                }, 400);
+                });
             } else {
                 console.log(0);
                 sidebar.animate({
                     marginTop: 0
-                 }, 400);
+                 });
             }
         }
     });
